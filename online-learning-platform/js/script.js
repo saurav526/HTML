6b39dd7e9494c7791105568f83e13645964ad502
saurav526 <sauravtiwari2712005@gmail.com>
2026-01-25 00:16:17 +0530

    apply css and js

---
+++ online-learning-platform/js/script.js
@@ -0,0 +1,15 @@
+function loginUser() {
+    alert("Login successful (demo only)");
+}
+
+function registerUser() {
+    alert("Registration successful (demo only)");
+}
+
+function enrollCourse() {
+    alert("You have enrolled in this course!");
+}
+function submitAssignment() {
+    alert("Assignment submitted successfully!");
+}   
+
